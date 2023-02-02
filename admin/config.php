4595ca0df93910d29f017cbb2d9743779cbecf99
Andrey <agallardo@yandex.ru>
2023-02-02 19:35:02 +0300

    Task done. Modules: SIGMA messaging, MultiCurrency, viewElse

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/admin/');
+define('HTTP_CATALOG', 'http://opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/admin/');
+define('HTTPS_CATALOG', 'http://opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/admin/');
+define('DIR_SYSTEM', '/var/www/opencart/system/');
+define('DIR_IMAGE', '/var/www/opencart/image/');
+define('DIR_LANGUAGE', '/var/www/opencart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencart/system/config/');
+define('DIR_CACHE', '/var/www/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/system/storage/download/');
+define('DIR_LOGS', '/var/www/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart2.3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
